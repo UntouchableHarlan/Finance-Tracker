f593dd6b1c3d06967149274cb679cb78db407db6
Salomon <salomonpluviose@yahoo.com>
2016-08-31 12:57:04 -0400

    Users can now add stocks and it will append to the page to see what stock their tracking. Also added validations to see if user is already tracking a certain stock

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,25 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :stocks
+
+  def can_add?(ticker)
+    under_stock_limit? && !stock_already_added?(ticker)
+  end
+
+  def under_stock_limit?
+    if self.stocks.count > 6
+      return false
+    else
+      return true
+    end
+  end
+
+  def stock_already_added?(ticker)
+    if self.stocks.where(ticker: ticker).exists?
+      return true
+    else
+      return false
+    end
+  end
+
 end
